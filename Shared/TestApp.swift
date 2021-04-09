c6f8070a887bf397ef02038a5b3e9f388a22d01c
longficient <longvietnam.jsc@gmail.com>
2021-04-09 14:22:14 +0700

    Initial Commit

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by LongFicient on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
